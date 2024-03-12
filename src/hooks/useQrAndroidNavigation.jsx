03ebdf1d57eba9eed6fe714dd29dbbbbc696d051
fake-dp <fakecoko92@gmail.com>
2024-03-13 00:21:46 +0900

    안드로이드 흔들흔들 센서감지기능 구현

--- src/hooks/useQrAndroidNavigation.jsx
+++ src/hooks/useQrAndroidNavigation.jsx
@@ -1,46 +1,38 @@
 import React, { useEffect, useState } from 'react';
-import { accelerometer } from 'react-native-sensors';
 import { useNavigation } from '@react-navigation/native';
 import { useRecoilState } from 'recoil';
 import { QRState } from '../store/atom';
-import { debounce } from 'lodash'; // lodash의 debounce 함수를 사용합니다.
+import { accelerometer, setUpdateIntervalForType, SensorTypes } from 'react-native-sensors';
+import { Subscription } from 'rxjs';
 
-const useQrAndroidNavigation = () => {
+const useQrAndroidNavigation=()=> {
     const navigation = useNavigation();
-    const [qr] = useRecoilState(QRState); // setQR 사용하지 않으므로 제거
-    const [subscription, setSubscription] = useState(null);
+    const [qr] = useRecoilState(QRState);
+    const [lastShake, setLastShake] = useState(Date.now());
 
     useEffect(() => {
-        // 디바운스된 흔들림 감지 함수를 정의합니다.
-        const handleShake = debounce(({ x, y, z }) => {
-            const totalAcceleration = Math.sqrt(x * x + y * y + z * z);
-            if (totalAcceleration > 3) { // 임계값 조건
-                console.log('Device shaken hard');
-                if (qr) { // QR 상태 확인
+        setUpdateIntervalForType(SensorTypes.accelerometer, 100);
+
+        let subscription;
+        if (qr) { 
+            subscription = accelerometer.subscribe(({ x, y, z }) => {
+                const current = Date.now();
+                const max = Math.max(x, y, z);
+                if (max > 12 && current - lastShake > 1000) {
+                    setLastShake(current);
                     navigation.navigate('Scan');
                 }
-            }
-        }, 500, { leading: true, trailing: false }); // 디바운스 설정
-
-        // QR 상태가 true일 때만 센서를 구독합니다.
-        if (qr && !subscription) {
-            const newSubscription = accelerometer.subscribe(handleShake);
-            setSubscription(newSubscription);
-        } else if (!qr && subscription) {
-            // QR 상태가 false이거나 구독을 해제해야 할 때
-            subscription.unsubscribe();
-            setSubscription(null);
+            }, error => {
+                console.log("The sensor is not available");
+            });
         }
 
-        // 컴포넌트가 언마운트될 때 센서 구독을 해제합니다.
         return () => {
-            if (subscription) {
+            if (subscription instanceof Subscription) {
                 subscription.unsubscribe();
             }
         };
-    }, [qr, navigation]); // 의존성 배열에 qr 추가
-
-    return null; // 이 훅은 UI를 렌더링하지 않습니다.
+    }, [qr, navigation, lastShake]);
 };
 
 export default useQrAndroidNavigation;
